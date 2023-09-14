f8aec24a6e0590761259e43916197b54853359ad
farahisJoy <farahelaydi9@gmail.com>
2023-09-14 11:26:13 +0000

    update

--- src/main/java/com/example/Authentication/Controller/AuthenticationConroller.java
+++ src/main/java/com/example/Authentication/Controller/AuthenticationConroller.java
@@ -2,17 +2,13 @@ package com.example.Authentication.Controller;
 
 import com.example.Authentication.Repository.PostRepository;
 import com.example.Authentication.Repository.SiteUserRepository;
-import com.example.Authentication.exception.userNotFound;
-import com.example.Authentication.models.Post;
 import com.example.Authentication.models.SiteUser;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
